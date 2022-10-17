1b03c69e7f211450f1be18c7bdb05bf231cd8ed2
davidpierce24 <pie2ce@gmail.com>
2022-10-17 17:32:25 -0400

    loading updates

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -6,11 +6,11 @@ import Navbar from "../components/Navbar";
 const Main = props => {
     const [load, setLoad] = useState(false);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLoad(true)
-        }, 2000 )
-    }, [])
+    // useEffect(() => {
+    //         setTimeout(() => {
+    //         setLoad(true)
+    //     }, 2000 )
+    // }, [])
 
 
     const backgrounds = ["Yasuo_18", "KogMaw_28", "Lux_40", "LeeSin_11", "Nunu_16", "Vayne_11", "Garen_13", "Gwen_1", "DrMundo_3", "Jinx_37" ]
@@ -33,7 +33,8 @@ const Main = props => {
                 {/* <img src="https://1l84rj2eepdd3xktbl163vrw-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/riotwhitep.png" alt="pic" className="m-auto w-sm sm:w-lg pt-60 " /> */}
                 <p style={{fontFamily: 'Josefin Sans'}} className="m-auto w-sm sm:w-lg pt-80 pb-10 drop-shadow-md text-white text-6xl sm:text-8xl lg:text-9xl ">Champ View</p>
 
-                <div className={load ? "": "hidden"}>
+                {/* <div className={load ? "": "hidden"}> */}
+                <div>
                     <AllChampionsList  /> 
                     <p className="text-white p-10 text-xs">Champ View isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</p>
                 </div>
